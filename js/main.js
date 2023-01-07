cedd09c83122a01b4d9c3f7c978c7ee17cf293aa
nguyenthaongoc3212 <114852146+nguyenthaongoc3212@users.noreply.github.com>
2023-01-07 20:50:18 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,187 @@
+const playBtn = document.querySelector(".play"),
+    skipForward = document.querySelector(".skip-forward"),
+    skipBack = document.querySelector(".skip-back"),
+
+    progressBarContainer = document.querySelector('.progress'),
+    progressBar = document.querySelector('.progress-bar'),
+    progressHead = document.querySelector('.progress-head'),
+
+    currentTimeHtml = document.querySelector(".current-time"),
+    durationHtml = document.querySelector(".duration"),
+
+    playIcon = document.querySelector('.fa-play'),    
+    img = document.querySelector('.img'),
+    title = document.querySelector(".audio-title"),
+    singer = document.querySelector(".audio-singer");
+
+this.tracks = [
+    {
+         name: "Hòa Nhịp Giáng Sinh",
+         artist: "MIN",
+         cover: "/img/tải xuống.jfif",
+         source: "/Music/Hoa Nhip Giang Sinh - Min.mp3",
+             },
+     {
+        name: "Có Hẹn Với Thanh Xuân",
+        artist: "SUNI HẠ LINH - HOÀNG DŨNG  ",
+        cover: "/img/tải xuống (1).jfif",
+        source: "/Music/Co Hen Voi Thanh Xuan - Suni Ha Linh_ Ho.mp3",
+             },
+     {
+      name: "Đi Để Trở Về",
+      artist: "SOOBIN HOÀNG SƠN ",
+      cover: "/img/tải xuống (2).jfif",
+      source: "/Music/DiDeTroVe-SoobinHoangSon-4726882.mp3",
+             },
+             {
+                name: "Bao Tiền Một Mớ Bình Yên",
+                artist: "14 CASPER-BON",
+                cover: "/img/bao tien mot mo binh yen.jfif",
+                source: "/Music/BaoTienMotMoBinhYen-14CasperBon-7463948.mp3 "
+             }   
+         ];
+
+
+let audio = null,
+    barWidth = null,
+    duration = null,
+    currentTime = null,
+    isTimerPlaying = false,
+    currentTrackIndex = 0,
+    currentTrack = tracks[0];
+
+
+audio = new Audio();
+audio.src = currentTrack.source;
+img.src = currentTrack.cover;
+title.innerText = currentTrack.name;
+singer.innerText = currentTrack.artist;
+
+playBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        isTimerPlaying = true;
+    } else {
+        audio.pause();
+        isTimerPlaying = false;
+    }
+});
+
+progressBarContainer.addEventListener('click', (x) => {
+    let maxduration = audio.duration;
+    let position = x.pageX - progressBarContainer.offsetLeft;
+    let percentage = (100 * position) / progressBarContainer.offsetWidth;
+    if (percentage > 100) percentage = 100;
+    if (percentage < 0) percentage = 0;
+    barWidth = percentage + "%";
+
+    audio.currentTime = (maxduration * percentage) / 100;
+    progressBar.style.width = `${barWidth}%`;
+    progressHead.style.setProperty("left", `${barWidth}%`);
+    img.src = currentTrack.cover;
+});
+
+
+skipForward.addEventListener('click', () => {
+
+    if (currentTrackIndex < tracks.length - 1) {
+        currentTrackIndex++;
+    } else {
+        currentTrackIndex = 0;
+    }
+
+    currentTrack = tracks[currentTrackIndex];
+
+    audio.src = currentTrack.source;
+    img.src = currentTrack.cover;
+    title.innerText = currentTrack.name;
+    singer.innerText = currentTrack.artist;
+
+    barWidth = 0;
+    progressBar.style.width = `${barWidth}%`;
+    progressHead.style.setProperty("left", `${barWidth}%`);
+    currentTimeHtml.innerText = `0:00`;
+    durationHtml.innerText = `0:00`;
+
+    audio.currentTime = 0;
+    audio.src = currentTrack.source;
+
+    setTimeout(() => {
+        if (isTimerPlaying) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }, 300);
+});
+
+skipBack.addEventListener('click', () => {
+    if (currentTrackIndex > 0) {
+        currentTrackIndex--;
+    } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+    }
+    currentTrack = tracks[currentTrackIndex];
+
+    audio.src = currentTrack.source;
+    img.src = currentTrack.cover;
+    title.innerText = currentTrack.name;
+    singer.innerText = currentTrack.artist;
+
+    barWidth = 0;
+    progressBar.style.width = `${barWidth}%`;
+    progressHead.style.setProperty("left", `${barWidth}%`);
+    currentTimeHtml.innerText = `0:00`;
+    durationHtml.innerText = `0:00`;
+
+    audio.currentTime = 0;
+    audio.src = currentTrack.source;
+
+    setTimeout(() => {
+        if (isTimerPlaying) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }, 300);
+});
+
+audio.ontimeupdate = function () {
+    if (audio.duration) {
+        barWidth = (100 / audio.duration) * audio.currentTime;
+
+        let durmin = Math.floor(audio.duration / 60);
+        let dursec = Math.floor(audio.duration - durmin * 60);
+        let curmin = Math.floor(audio.currentTime / 60);
+        let cursec = Math.floor(audio.currentTime - curmin * 60);
+
+        if (durmin < 10) durmin = "0" + durmin;
+
+        if (dursec < 10) dursec = "0" + dursec;
+
+        if (curmin < 10) curmin = "0" + curmin;
+
+        if (cursec < 10) cursec = "0" + cursec;
+
+        duration = durmin + ":" + dursec;
+        currentTime = curmin + ":" + cursec;
+
+        progressBar.style.width = `${barWidth}%`;
+        progressHead.style.setProperty("left", `${barWidth}%`)
+        currentTimeHtml.innerText = `${currentTime}`;
+        durationHtml.innerText = `${duration}`;
+
+        if (isTimerPlaying) {
+            playIcon.classList.remove('fa-play');
+            playIcon.classList.add('fa-pause');
+
+
+        } else {
+            playIcon.classList.add('fa-play');
+            playIcon.classList.remove('fa-pause');
+        }
+    }
+};
+
+audio.onended = function () { };
+
